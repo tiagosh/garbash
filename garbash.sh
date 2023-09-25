808f1c4e1adcdef80ac2c82b945ede5946b2d46a
Tiago Salem Herrmann <tiagosh@gmail.com>
2023-09-24 22:53:53 -0300

    Improve runtime error handling

--- garbash.sh
+++ garbash.sh
@@ -4,27 +4,24 @@ declare -gA closure_scopes; declare -g scope_counter=0
 declare -gA scope_value_0; declare -gA scope_kind_0; declare -gA value_cache
 if ! type jq >/dev/null 2>&1; then echo jq not found; exit 1; fi
 
-
-try_get_value_from_cache() {
-    local start=$1
-    result=${value_cache["$start"]}
-    if [ -z "$result" ]; then
-        parse_json .value
-        value_cache["$start"]=$result
-    fi
+runtime_error() {
+    echo -e "\e[0;31mRuntime error: $1\e[0m" 1>&2; exit 1;
 }
 
-try_get_text_from_cache() {
-    local start=$1
-    result=${value_cache["$start"]}
-    if [ -z "$result" ]; then
-        parse_json .text
-        value_cache["$start"]=$result
-    fi
+assert_kind() {
+    [ $result_kind != "$1" ] && runtime_error "Invalid BinaryOp kinds"
 }
 
 parse_json() {
     result=$(jq -c -r "$1")
+}
+
+try_get_from_cache() {
+    local start=$1; local prop=$2
+    result=${value_cache["$start"]}
+    [ -n "$result" ] && return
+    parse_json $prop
+    value_cache["$start"]=$result
 }
 
 get_var_from_scope() {
@@ -32,12 +29,11 @@ get_var_from_scope() {
     eval "result=\${scope_value_$scope_id['$name']}"
     eval "result_kind=\${scope_kind_$scope_id['$name']}"
     eval "local type=\${scope_kind_$scope_id[$name]}"
-    if [ "$type" = "Tuple" ]; then
-        eval "result_tuple[0]=\${scope_kind_$scope_id['$name-first']}"
-        eval "result_tuple[1]=\${scope_value_$scope_id['$name-first']}"
-        eval "result_tuple[2]=\${scope_kind_$scope_id['$name-second']}"
-        eval "result_tuple[3]=\${scope_value_$scope_id['$name-second']}"
-    fi
+    [ "$type" != "Tuple" ] && return
+    eval "result_tuple[0]=\${scope_kind_$scope_id['$name-first']}"
+    eval "result_tuple[1]=\${scope_value_$scope_id['$name-first']}"
+    eval "result_tuple[2]=\${scope_kind_$scope_id['$name-second']}"
+    eval "result_tuple[3]=\${scope_value_$scope_id['$name-second']}"
 }
 
 set_var_in_scope() {
@@ -57,18 +53,18 @@ set_tuple_in_scope() {
 }
 
 eval_function() {
-    try_get_value_from_cache $3 <<< $1
+    try_get_from_cache $3 .value <<< $1
     evaluate "$result" $2
 }
 
 eval_print() {
     local text
-    try_get_value_from_cache $3 <<< $1
+    try_get_from_cache $3 .value <<< $1
     evaluate "$result" $2
     case $result_kind in
-    Bool) [ $result -eq 0 ] &&  text=false || text=true ;;
+    Bool) [ $result -eq 0 ] && text=false || text=true ;;
     Tuple) text="(${result_tuple[1]}, ${result_tuple[3]})" ;;
-    Function) echo "<#closure>" ;;
+    Function) text="<#closure>" ;;
     *) text=$result ;;
     esac
     echo $text
@@ -82,7 +78,7 @@ eval_call() {
     parse_json '.parameters[].text' <<< $func_body
     set -- $result
     local params=$result; local num_params=$#
-    [ $num_args -ne $num_params ] && echo "invalid number of args: $name" && exit 1
+    [ $num_args -ne $num_params ] && runtime_error "invalid number of args: $name"
     # recover original scope of this closure
     local func_id=$(cksum <<< $func_body)
     local closure_scope_id=${closure_scopes["$func_id"]}
@@ -102,18 +98,18 @@ eval_call() {
 }
 
 eval_var() {
-    try_get_text_from_cache $3 <<< $1
+    try_get_from_cache $3 .text <<< $1
     local scope_id=$2
     get_var_from_scope $result $scope_id
 }
 
 eval_int_string() {
-    try_get_value_from_cache $2 <<< $1
+    try_get_from_cache $2 .value <<< $1
 }
 
 eval_bool() {
-    try_get_value_from_cache $2 <<< $1
-    [ $result = "true" ] && result=1 || result=0; result_kind=Bool
+    try_get_from_cache $2 .value <<< $1
+    [ ! $result = "true" ]; result=$?; result_kind=Bool
 }
 
 eval_tuple() {
@@ -133,39 +129,52 @@ eval_binary() {
     local term="$1"; local scope_id=$2; local tmp_lhs; local tmp_rhs; local op
     parse_json .lhs,.rhs,.op <<< $term
     { read tmp_lhs;  read tmp_rhs; read op; } <<< $result
-
-    evaluate "$tmp_lhs" $scope_id
-    local lhs=$result; local lhs_kind=$result_kind
-    evaluate "$tmp_rhs" $scope_id
-    local rhs=$result; local rhs_kind=$result_kind
+    evaluate "$tmp_lhs" $scope_id; local lhs=$result; local lhs_kind=$result_kind
+    evaluate "$tmp_rhs" $scope_id; local rhs=$result; local rhs_kind=$result_kind
     if [ $lhs_kind = $rhs_kind ]; then
         result_kind=$lhs_kind
-    elif [ $lhs_kind = "Str" -o $rhs_kind = "Str" ]; then
+    elif [ $lhs_kind = "Str" -o $rhs_kind = "Int" ] || [ $lhs_kind = "Int" -o $rhs_kind = "Str" ]; then
         result_kind=Str
     else
-        echo "BinaryOp not support for $lhs_kind $op $rhs_kind"; exit 1
+        runtime_error "BinaryOp not support for $lhs_kind $op $rhs_kind"
     fi
     case $op in
     Add)
         case $result_kind in
             Str) result="$lhs$rhs" ;;
             Int) result=$(($lhs + $rhs)) ;;
-            *) echo "Add BinaryOp not support for $lhs_kind + $rhs_kind"; exit 1 ;;
+            *) runtime_error "Add BinaryOp not support for $lhs_kind + $rhs_kind" ;;
         esac
     ;;
-    Sub) result=$(($lhs - $rhs)) ;;
-    Mul) result=$(($lhs * $rhs)) ;;
-    Div) result=$(($lhs / $rhs)) ;;
-    Rem) result=$(($lhs % $rhs)) ;;
-    Eq) result=$(($lhs == $rhs)); result_kind=Bool ;;
-    Lt) result=$(($lhs < $rhs)); result_kind=Bool ;;
-    Gte) result=$(($lhs >= $rhs)); result_kind=Bool ;;
-    Gt) result=$(($lhs > $rhs)); result_kind=Bool ;;
-    Lte) result=$(($lhs <= $rhs)); result_kind=Bool ;;
-    Neq) [ "$lhs" != "$rhs" ] && result=1 || result=0; result_kind=Bool ;;
-    And) [ "$lhs" != "0" -a "$rhs" != "0" ] && result=1 || result=0; result_kind=Bool ;;
-    Or) [ "$lhs" != "0" -o "$rhs" != "0" ] && result=1 || result=0; result_kind=Bool ;;
-    *) echo $op not implemented; exit 1 ;;
+    Sub) assert_kind Int; result=$(($lhs - $rhs)) ;;
+    Mul) assert_kind Int; result=$(($lhs * $rhs)) ;;
+    Div) assert_kind Int; result=$(($lhs / $rhs)) ;;
+    Rem) assert_kind Int; result=$(($lhs % $rhs)) ;;
+    Eq|Neq)
+        if [ $result_kind == "Int" ] || [ $result_kind == "Bool" ]; then
+            [ $lhs -eq $rhs ]; result=$?
+        elif [ $result_kind = "Str" ]; then
+            [ $lhs = $rhs ]; result=$?
+        else
+            runtime_error "Can't compare $lhs_kind and $rhs_kind"
+        fi
+        [ $op == "Eq" ] && result=$(($result == 0))
+        result_kind=Bool
+    ;;
+    Lt) assert_kind Int; result=$(($lhs < $rhs)); result_kind=Bool ;;
+    Gte) assert_kind Int; result=$(($lhs >= $rhs)); result_kind=Bool ;;
+    Gt) assert_kind Int; result=$(($lhs > $rhs)); result_kind=Bool ;;
+    Lte) assert_kind Int; result=$(($lhs <= $rhs)); result_kind=Bool ;;
+    And|Or)
+        assert_kind Bool
+        if [ $op = "And" ]; then
+            [ "$lhs" = "1" -a "$rhs" = "1" ]
+        else
+            [ "$lhs" = "1" -o "$rhs" = "1" ]
+        fi
+        result=$(($? == 0)); result_kind=Bool
+    ;;
+    *) runtime_error "$op not implemented";;
     esac
 }
 
@@ -174,18 +183,16 @@ eval_if() {
     parse_json .condition,.then,.otherwise <<< $term
     { read condition; read then; read otherwise; } <<< $result
     evaluate "$condition" $scope_id
-    if [ $result -eq 1 ]; then
-        evaluate "$then" $scope_id
-    else
-        evaluate "$otherwise" $scope_id
-    fi
+    [ $result_kind != "Bool" ] && runtime_error "Invalid variable kind for If term"
+    [ $result -eq 1 ] && term_to_eval=$then || term_to_eval=$otherwise
+    evaluate "$term_to_eval" $scope_id
 }
 
 eval_let() {
     local term=$1; local scope_id=$2; local name; local kind;
     parse_json .name.text,.value.kind <<< $term
     { read name; read kind; } <<< $result
-    try_get_value_from_cache $3 <<< $term
+    try_get_from_cache $3 .value <<< $term
     if [ "$kind" != "Function" ]; then
         evaluate "$result" $scope_id
         kind=$result_kind
@@ -201,17 +208,17 @@ eval_let() {
 }
 
 eval_first() {
-    try_get_value_from_cache $3 <<< $1
+    try_get_from_cache $3 .value <<< $1
     evaluate "$result" $2
-    result=${result_tuple[1]}
-    result_kind=${result_tuple[0]}
+    [ $result_kind != "Tuple" ] && runtime_error "called first() on a non Tuple"
+    result=${result_tuple[1]}; result_kind=${result_tuple[0]}
 }
 
 eval_second() {
-    try_get_value_from_cache $3 <<< $1
+    try_get_from_cache $3 .value <<< $1
     evaluate "$result" $2
-    result=${result_tuple[3]}
-    result_kind=${result_tuple[2]}
+    [ $result_kind != "Tuple" ] && runtime_error "called second() on a non Tuple"
+    result=${result_tuple[3]}; result_kind=${result_tuple[2]}
 }
 
 evaluate() {
@@ -232,11 +239,9 @@ evaluate() {
     Bool) eval_bool "$term" $start ;;
     Let)
         eval_let "$term" $scope_id $start
-        if [ "$next" != "null" ]; then
-            evaluate "$next" $scope_id
-        fi
+        [ "$next" != "null" ] && evaluate "$next" $scope_id
     ;;
-    *) echo undefined kind $kind; exit 1 ;;
+    *) runtime_error "undefined kind $kind" ;;
     esac
 }
 AST=${1:-/var/rinha/source.rinha.json}
